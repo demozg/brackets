51c7f00f1b2b6c66b4c1acd14a658e1eb3d623f0
Дмитрий <kuzmikd@mail.ru>
2023-02-18 12:42:16 +0300

    feat:add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.join('').replace(/,/g, '');
+    let arr = [];
+    for (let bracket of str) {
+        let bracketsNumber = brackets.indexOf(bracket)
+        if (bracketsNumber % 2 === 0) {              
+            if (bracket === brackets[bracketsNumber + 1] && arr[arr.length - 1] === bracketsNumber){
+                arr.pop();
+            } else if (bracket === brackets[bracketsNumber + 1] && arr[arr.length - 1] !== bracketsNumber) {
+                arr.push(bracketsNumber)
+            }
+            else{
+                arr.push(bracketsNumber)
+            }
+        } 
+        else {
+            if (arr.pop() !== bracketsNumber - 1){
+                return false;
+            }
+        }
+    }
+    return arr.length === 0
 }
